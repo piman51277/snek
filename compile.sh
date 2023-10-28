530c1b4c9d149a9fe43418ca5dbc01690c3a8442
piman51277 <piman.dev@gmail.com>
2023-10-28 14:34:46 -0400

    update compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -o a src/*.cpp -O2 -I/usr/include/SDL2 -D_REENTRANT -lSDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2_image
+g++ -o bin/a src/*.cpp -O2 -I/usr/include/SDL2 -D_REENTRANT -lSDL2 -L/usr/lib/x86_64-linux-gnu -lSDL2_image
